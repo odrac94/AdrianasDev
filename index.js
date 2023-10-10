0efa340d36215094108cfe24091596ba9faa91d7
Ricardo Delfin <richardo.delfin94@gmail.com>
2023-10-09 19:12:34 -0700

    cree el archivo index.js para generar el html dinamicamente con javascript

--- index.js
+++ index.js
@@ -36,3 +36,43 @@ proyectosArr = [
         enlace: 'adriana-gallardo/adriana-gallardo.html'
     }
 ];
+
+
+const proyectosDiv = document.querySelector('.proyectos');
+
+const proyectosGridDiv = document.createElement('div');
+    proyectosGridDiv.classList.add('contenedor', 'grid');
+
+for (const proyecto of proyectosArr) {
+    const elementoDiv = document.createElement('div');
+    elementoDiv.classList.add('elemento');
+
+    const enlaceCartaAnchor = document.createElement('a');
+    enlaceCartaAnchor.setAttribute('href', proyecto.enlace);
+
+    const cartaDiv = document.createElement('div');
+    cartaDiv.classList.add('carta');
+
+    const cartaImgDiv = document.createElement('div');
+    cartaImgDiv.classList.add('carta__img');
+    cartaImgDiv.style.backgroundImage = `url(${proyecto.img})`; // Usa style.backgroundImage
+
+    const cartaContenidoDiv = document.createElement('div');
+    cartaContenidoDiv.classList.add('carta__contenido');
+
+    const marcaH2 = document.createElement('h2');
+    marcaH2.textContent = proyecto.marca; // Agrega texto al elemento h2
+
+
+    const descripcionP = document.createElement('p');
+    descripcionP.textContent = proyecto.descripcion; // Agrega texto al elemento p
+
+    proyectosDiv.appendChild(proyectosGridDiv);
+    proyectosGridDiv.appendChild(elementoDiv);
+    elementoDiv.appendChild(enlaceCartaAnchor);
+    enlaceCartaAnchor.appendChild(cartaDiv);
+    cartaDiv.appendChild(cartaImgDiv);
+    cartaDiv.appendChild(cartaContenidoDiv);
+    cartaContenidoDiv.appendChild(marcaH2);
+    cartaContenidoDiv.appendChild(descripcionP);
+};
