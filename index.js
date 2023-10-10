0ae33738983c00b08ba3ea925eff3afd984b0586
Ricardo Delfin <richardo.delfin94@gmail.com>
2023-10-09 18:29:47 -0700

    cree la interfaz de la pagina del home

---
+++ index.js
@@ -0,0 +1,38 @@
+proyectosArr = [
+    {
+        img: 'https://www.wordstream.com/wp-content/uploads/2022/07/website-examples-feature.jpg',
+        marca: 'Adriana Gallardo',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        enlace: 'adriana-gallardo/adriana-gallardo.html'
+    },
+    {
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png',
+        marca: "Adriana's Insurance",
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        enlace: 'adriana-gallardo/adriana-gallardo.html'
+    },
+    {
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png',
+        marca: "Adriana's Immigration Services",
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        enlace: 'adriana-gallardo/adriana-gallardo.html'
+    },
+    {
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png',
+        marca: 'AGI Business',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        enlace: 'adriana-gallardo/adriana-gallardo.html'
+    },
+    {
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png',
+        marca: 'AGNC Media Group',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        enlace: 'adriana-gallardo/adriana-gallardo.html'
+    },
+    {
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png',
+        marca: 'AGLA',
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        enlace: 'adriana-gallardo/adriana-gallardo.html'
+    }
+];
